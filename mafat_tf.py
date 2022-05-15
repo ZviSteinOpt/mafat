bfe0e5a21d20ef3f72df55f9b3aa219ce1d35382
ZviSteinOpt <tzvist3@gmail.com>
2022-05-15 09:51:46 +0300

    massege

--- mafat_tf.py
+++ mafat_tf.py
@@ -6,12 +6,15 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 data = pd.read_csv('/Users/zvistein/Downloads/mafat_wifi_challenge_training_set_v1.csv')
+train = data.sample(frac=0.8,random_state=0)
+test  = data.drop(train.index)
 l = (len(data.RSSI_Right)-len(data.RSSI_Right)%360)
 b =  data.RSSI_Right[0:l]
 a =  data.RSSI_Left[0:l]
-a = np.append(a, b)
+a = np.append(a,b)
 rss =  a.reshape(int(l/360*2),360)
 rss_n = np.zeros((int(l/360),360,2))
+rss_n = pd.DataFrame(rss_n)
 rss_n[:,:,0] = rss[0:int(l/360),:]
 rss_n[:,:,1] = rss[int(l/360):,:]
 
@@ -27,7 +30,6 @@ train_data = []
 for i in range(len(rss_n)):
    train_data.append([rss_n[i], gt[i]])
 
-model = keras.models.Sequential()
 
 class cnnMA(keras.Model):
     def __init__(self):
