953e20c3af237f57c905550f2f1effa34084e0b0
ZviSteinOpt <tzvist3@gmail.com>
2022-05-19 13:59:52 +0300

    massege

--- mafat_cnn.py
+++ mafat_cnn.py
@@ -6,17 +6,20 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader,WeightedRandomSampler,random_split
+import time
 
+if torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
 
-def get_loader(DataSets):
+def get_loader(DataSets,class_weights):
 
     # oversampling the unbalanced data
-    class_weights = [3,1] # the balance ratio is 1 to 5
+    #class_weights = [3,3,1,3] # the balance ratio is 1 to 5
     sample_weights = [0]*len(DataSets)
 
     for idx, (data,label) in enumerate(DataSets):
-        if label>0:
-         label = 1
         class_weight = class_weights[int(label)]
         sample_weights[idx] = class_weight
 
@@ -24,7 +27,7 @@ def get_loader(DataSets):
 
     # Dataloader
     train_loader = DataLoader(DataSets,
-        batch_size=40,sampler = sampler)
+        batch_size=100,sampler = sampler)
 
     return train_loader
 
@@ -32,34 +35,61 @@ def get_loader(DataSets):
 
 data = pd.read_csv('/Users/zvistein/Downloads/mafat_wifi_challenge_training_set_v1.csv')
 wind = 360
-l = (len(data.RSSI_Right)-len(data.RSSI_Right)%wind)
-b =  data.RSSI_Right[0:l]
-a =  data.RSSI_Left[0:l]
+rm = 3
+Dr = data.RSSI_Right[data.Room_Num == rm]
+Dl = data.RSSI_Left[data.Room_Num == rm]
+La = data.Num_People[data.Room_Num == rm]
+l = (len(Dr)-len(Dr)%wind)
+b =  Dr[0:l]
+a =  Dl[0:l]
 a = np.append(a,b)
 rss =  a.reshape(int(l/wind*2),wind)
-rss_n = np.zeros((int(l/wind),wind,4))
-rss_n[:,:,0] = (rss[0:int(l/wind),:]-np.min(rss[0:int(l/wind),:]))/(np.max(rss[0:int(l/wind),:])-np.min(rss[0:int(l/wind),:]))
-rss_n[:,:,1] = (rss[int(l/wind):,:]-np.min(rss[int(l/wind):,:]))/(np.max(rss[int(l/wind):,:])-np.min(rss[int(l/wind):,:]))
-# adding the substract betw the lobs
-mn = np.min(rss[int(l/wind):,:]+rss[0:int(l/wind),:])
-mx = np.max(rss[int(l/wind):,:]+rss[0:int(l/wind),:])
-rss_n[:,:,3] = (rss[int(l/wind):,:]+rss[0:int(l/wind),:]-mn)/(mx-mn)
-mn = np.min(rss[int(l/wind):,:]-rss[0:int(l/wind),:])
-mx = np.max(rss[int(l/wind):,:]-rss[0:int(l/wind),:])
-rss_n[:,:,2] = (rss[int(l/wind):,:]-rss[0:int(l/wind),:]-mn)/(mx-mn)
+rss_n = np.zeros((int(l/wind),wind,1))
+stdz = []
+for i in range(len(rss_n)):
+    mn = np.mean(rss[i,:])
+    mx0 = np.std(rss[i,:])
+    rss_n[i,:,0] = (rss[i,:]-mn)/mx0
+    mn = np.mean(rss[i+int(l/wind),:])
+    mx1 = np.std(rss[i+int(l/wind),:])
+    rss_n[i,:,0] = (rss[i+int(l/wind),:]-mn)/mx1
 
+    # adding the substract betw the lobs
+    temp = rss[i,:]+rss[i+int(l/wind),:]
+    mn = np.mean(temp)
+    mx2 = np.std(temp)
+    rss_n[i,:,0] = (temp-mn)/mx2
+    temp = rss[i,:]-rss[i+int(l/wind),:]
+    mn = np.mean(temp)
+    mx3 = np.std(temp)
+    rss_n[i,:,0] = (temp-mn)/mx3
+    if mx3==0 or mx1==0:
+        stdz.append(i)
+    else:
+        gg = 1
 
-b = data.Num_People[0:l]
+rss_n = np.delete(rss_n,stdz,0)
+
+b = La[0:l]
 num = b.values.reshape(int(l/360),360)
 gt = np.zeros(int(l/360))
 for i in np.arange(0,int(l/360)):
  n = num[i,:]
  b = Counter(n)
- gt[i] = np.sign(b.most_common(1)[0][0])
+ gt[i] = b.most_common(1)[0][0]
 
+weit = [0.25/(sum(gt==0)/len(gt)) ,0.25/(sum(gt==1)/len(gt)) ,0.25/(sum(gt==2)/len(gt)) ,0.25/(sum(gt==3)/len(gt))]
+weit = np.round(weit/min(weit))
 train_data = []
-for i in range(10):#len(rss_n)/100):
+train_data_of = []
+flag = np.zeros(4)
+count = 0
+for i in range(len(rss_n)):
    train_data.append([rss_n[i], gt[i]])
+   flag[int(gt[i])] = flag[int(gt[i])]+1
+   if flag[int(gt[i])]<100:
+       train_data_of.append([rss_n[i], gt[i]])
+       count = count+1
 
 
 class FN(nn.Module):
@@ -68,26 +98,26 @@ class FN(nn.Module):
 
         # Output size after convolution filter
         # ((w-f+2P)/s) +1
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=12, kernel_size=[1,2], stride=1, padding=0)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=24, kernel_size=[1,2], stride=1, padding=0)
         # Shape= (b_s,12,50,50)
-        self.bn1 = nn.BatchNorm2d(num_features=12)
+        self.bn1 = nn.BatchNorm2d(num_features=24)
         # Shape= (b_s,12,50,50)
 
         # Input shape= (b_s,1,50,50)
 
-        self.fc1 = nn.Linear(in_features=4*360, out_features=36000)
+        self.fc1 = nn.Linear(in_features=360*1, out_features=36000)
         self.bn2 = nn.BatchNorm1d(36000)
         self.fc2 = nn.Linear(in_features=36000, out_features = 1000)
         self.bn3 = nn.BatchNorm1d(1000)
-        self.fc3 = nn.Linear(in_features=1000, out_features = 100)
-        self.bn4 = nn.BatchNorm1d(100)
-        self.fc4 = nn.Linear(in_features=100, out_features = 50)
-        self.bn5 = nn.BatchNorm1d(50)
-        self.fc5 = nn.Linear(in_features=50, out_features = 25)
-        self.bn6 = nn.BatchNorm1d(25)
-        self.fc6 = nn.Linear(in_features=25, out_features = 10)
-        self.bn7 = nn.BatchNorm1d(10)
-        self.fc7 = nn.Linear(in_features=10, out_features = 2)
+        self.fc3 = nn.Linear(in_features=1000, out_features = 1000)
+        self.bn4 = nn.BatchNorm1d(1000)
+        self.fc4 = nn.Linear(in_features=1000, out_features = 500)
+        self.bn5 = nn.BatchNorm1d(500)
+        self.fc5 = nn.Linear(in_features=500, out_features = 250)
+        self.bn6 = nn.BatchNorm1d(250)
+        self.fc6 = nn.Linear(in_features=250, out_features = 100)
+        self.bn7 = nn.BatchNorm1d(100)
+        self.fc7 = nn.Linear(in_features=100, out_features = 4)
 
         self.relu = nn.ReLU()
         self.Lrelu = nn.LeakyReLU()
@@ -95,13 +125,12 @@ class FN(nn.Module):
         # Feed forwad function
 
     def forward(self, input):
-        # tmpInput = input
         # output = self.conv1(input)
         # output = self.bn1(output)
         # output = self.Lrelu(output)
 
-        output = input.view(-1, 4*360)
-        # input  = input.view(-1, 360 * 2)
+        output = input.view(-1, 1*360)
+        # output  = output.view(-1, 12*360 * 3)
         # output = torch.cat(( output.permute(1,0) , input.permute(1,0) ),0)
         # output = output.permute(1, 0)
         output = self.fc1(output)
@@ -129,27 +158,28 @@ class FN(nn.Module):
         return output
 
 
-test_count  = len(rss_n)//7
+test_count  = len(rss_n)//9
 train_count = len(rss_n)-test_count
-test_count  = 7
-train_count = 3
-train_sets, test_setes = random_split(train_data,[train_count,test_count])
+# train_count = np.floor(count*0.8)
+# test_count  = count-np.floor(count*0.8)
 
+train_sets, test_setes = random_split(train_data,[train_count,test_count])
+# train_sets, test_setes = random_split(train_data_of,[int(train_count),int(test_count)])
 test_loader = DataLoader(test_setes,
     batch_size=500, shuffle=True)
-train_loader = get_loader(train_sets)
+train_loader = get_loader(train_sets,weit)
 # train_loader = DataLoader(train_sets,
 #     batch_size=250, shuffle=True)
 
 torch.manual_seed(3407)
-model = FN(num_classes=1)
+model = FN(num_classes=1).to(device)
 
-optimizer     = torch.optim.Adam(model.parameters(), lr=0.1)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=(0.9))
+optimizer     = torch.optim.Adam(model.parameters(), lr=0.01)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=150, gamma=(0.9))
 
 loss_function = nn.CrossEntropyLoss()
 
-num_epochs = 100
+num_epochs = 1000
 s_loss = torch.zeros(num_epochs*len(train_loader))
 idx = 0
 
@@ -161,12 +191,15 @@ for epoch in range(num_epochs):
     train_loss = 0.0
 
     for i, (seq, labels) in enumerate(train_loader):
-        labels[labels > 0] = 1
         optimizer.zero_grad()
         seq = seq[None, :]
         seq = seq.permute(1, 0, 2, 3)
-        outputs = model(seq.float())
-        loss = loss_function(outputs, labels.long())
+        seq = seq.float()
+        seq = seq.to(device)
+        outputs = model(seq)
+        labels = labels.long()
+        labels = labels.to(device)
+        loss = loss_function(outputs, labels)
         s_loss[idx] = loss
         idx = idx+1
         loss.backward()
@@ -184,11 +217,13 @@ for epoch in range(num_epochs):
 
     test_accuracy = 0.0
     for i, (seq, labels) in enumerate(test_loader):
-        labels[labels > 0] = 1
         seq = seq[None, :]
         seq = seq.permute(1, 0, 2, 3)
-
-        outputs = model(seq.float())
+        seq = seq.float()
+        seq = seq.to(device)
+        labels = labels.long()
+        labels = labels.to(device)
+        outputs = model(seq)
 
         _, prediction = torch.max(outputs.data, 1)
         test_accuracy += int(torch.sum(prediction == labels.data))
@@ -196,3 +231,6 @@ for epoch in range(num_epochs):
 
     print('Epoch: ' + str(epoch) + ' Train Loss: ' + str(train_loss) + ' Train Accuracy: ' + str(
         train_accuracy) + ' Test Accuracy: ' + str(test_accuracy))
+
+
+
--- tf_d.py
+++ tf_d.py
@@ -1,74 +1,81 @@
-import numpy as np
-import pandas as pd
-from collections import Counter
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorboard.plugins.hparams import api as hp
-from imblearn.under_sampling import RandomUnderSampler
-from scipy import io
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+import math
 
 data = pd.read_csv('/Users/zvistein/Downloads/mafat_wifi_challenge_training_set_v1.csv')
-
-# orgenized the data 360X2
-wind = 360
-l = (len(data.RSSI_Right)-len(data.RSSI_Right)%wind)
-b =  data.RSSI_Right[0:l]
-a =  data.RSSI_Left[0:l]
-a = np.append(a,b)
-rss =  a.reshape(int(l/wind*2),wind)
-rss_n = np.zeros((int(l/wind),wind,4))
-rss_n[:,:,0] = (rss[0:int(l/wind),:]-np.min(rss[0:int(l/wind),:]))/(np.max(rss[0:int(l/wind),:])-np.min(rss[0:int(l/wind),:]))
-rss_n[:,:,1] = (rss[int(l/wind):,:]-np.min(rss[int(l/wind):,:]))/(np.max(rss[int(l/wind):,:])-np.min(rss[int(l/wind):,:]))
-# adding the substract betw the lobs
-mx = np.max(rss[int(l/wind):,:]-rss[0:int(l/wind),:])
-mn = np.min(rss[int(l/wind):,:]-rss[0:int(l/wind),:])
-rss_n[:,:,2] = (rss[int(l/wind):,:]-rss[0:int(l/wind),:]-mn)/(mx-mn)
-mx = np.max(rss[int(l/wind):,:]+rss[0:int(l/wind),:])
-mn = np.min(rss[int(l/wind):,:]+rss[0:int(l/wind),:])
-rss_n[:,:,3] = (rss[int(l/wind):,:]+rss[0:int(l/wind),:]-mn)/(mx-mn)
-
-# the labels as well
-b = data.Num_People[0:l]
-num = b.values.reshape(int(l/wind),wind)
-gt = np.zeros(int(l/wind))
-for i in np.arange(0,int(l/wind)):
- n = num[i,:]
- b = Counter(n)
- gt[i]  = b.most_common(1)[0][0]
+left  = data.RSSI_Left
+left = left.to_numpy()
+num_people = data.Num_People
+num_people = num_people.to_numpy()
+time = data.Time
+room_num = data.Room_Num
+id = data.Device_ID
+right = data.RSSI_Right
+right = right.to_numpy()
+dif   = right - left
 
 
-# undersumpeling treatment
-rus = RandomUnderSampler(sampling_strategy={0: 2110, 1: 988, 2: 3000, 3: 1096})
-d_s = np.zeros((len(rss_n),2))
-d_s[:,0] = np.arange(0,len(rss_n))
-x_u,y_u = rus.fit_resample(d_s,gt)
-x_u = x_u.astype(int)
-gt   = gt[x_u[:,0]]
-data_ds = rss_n[x_u[:,0],:]
+plt.plot(time[0:7632*360], dif[0:7632*360], 'bo')
+plt.grid()
+plt.show()
 
 
-# data loader
-train_size = int(0.8 * len(data_ds))
-full_dataset = tf.data.Dataset.from_tensor_slices((data_ds,gt)).shuffle(4000)
-#full_dataset = full_dataset.shuffle()
+from numpy.ma.core import count
+un_Ids     = np.unique(id) # Dividing by Device
+fin_size   = int(len(dif)/360) # Usable Size
+fin_dif    = dif[0:fin_size*360]
+fin_left   = left[0:fin_size*360]
+fin_right  = right[0:fin_size*360]
+fin_num_people = num_people[0:fin_size*360]
+fin_num_people[fin_num_people!=0] = 1
 
-train_ds = full_dataset.take(train_size).batch(5500)
-valid_ds = full_dataset.skip(train_size).batch(1200)
+
+fin_dif = fin_dif.reshape(7632,360)
+fin_dif = fin_dif.astype('float64')
+fin_num_people = fin_num_people.reshape(7632,360)
+fin_left = fin_left.reshape(7632,360)
+fin_right = fin_right.reshape(7632,360)
+fin_labels = np.zeros(fin_size)
+
+indy = np.array([])
+for i in range(0,fin_size):
+  temp = fin_num_people[i,:]
+  temp = Counter(temp)
+  num = temp.most_common(1)[0][0]
+  if num!=0:
+    fin_labels[i] = 1
+  else:
+    fin_labels[i] = 0
+  if np.std(fin_dif[i,:])!=0:
+    indy = np.append(indy,i)
+
+# Cleaning Trash Data std=0
+fin_left = fin_left[indy.astype('int'),:]
+fin_right = fin_right[indy.astype('int'),:]
+fin_labels = fin_labels[indy.astype('int')]
+fin_dif = fin_dif[indy.astype('int')]
+fin_dif = fin_dif.astype('float64')
+
+# Normalizing data. Subtracting mean, dividing std
+for i in range(0,len(indy)):
+  fin_dif[i,:]-=np.mean(fin_dif[i,:])
+  fin_dif[i,:]/=np.std(fin_dif[i,:])
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(360, 4)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
-])
-
-
-adm=tf.keras.optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'],optimizer=adm)
-
-for images, labels in train_ds:
-   for valid_images, valid_labels in valid_ds:
-      model.fit(images,labels,batch_size=25, epochs=10, )
-
+    tf.keras.layers.Conv1D(10,3,activation='relu',input_shape = (360,1)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512,activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dense(64,activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dense(1,activation='sigmoid')]
+)
+model.summary()
+model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics = ['accuracy'])
+model.fit(fin_dif,fin_labels,epochs = 10,verbose = 1,validation_split=0.1)
