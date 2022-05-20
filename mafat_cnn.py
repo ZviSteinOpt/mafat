f8c1d762b92abfdfc5e5c84365f5a746d13f646e
ZviSteinOpt <tzvist3@gmail.com>
2022-05-20 16:49:48 +0300

    massege

--- mafat_cnn.py
+++ mafat_cnn.py
@@ -27,7 +27,7 @@ def get_loader(DataSets,class_weights):
 
     # Dataloader
     train_loader = DataLoader(DataSets,
-        batch_size=100,sampler = sampler)
+        batch_size=50,sampler = sampler)
 
     return train_loader
 
@@ -36,8 +36,8 @@ def get_loader(DataSets,class_weights):
 data = pd.read_csv('/Users/zvistein/Downloads/mafat_wifi_challenge_training_set_v1.csv')
 all_data   = []
 all_labels = []
-weit       = [0,0,0,0]
-for ii in range(5):
+weit       = [0,0]
+for ii in range(1):
     wind = 360
     rm = 2
     Dr = data.RSSI_Right[data.Room_Num == rm]
@@ -47,27 +47,19 @@ for ii in range(5):
     b =  Dr[(ii*30):(l+(ii*30))]
     a =  Dl[(ii*30):(l+(ii*30))]
     a = np.append(a,b)
-    rss =  a.reshape(int(l/wind*2),wind)
-    rss_n = np.zeros((int(l/wind),wind,1))
+    rss =  a.reshape(2,len(a)//2)
+    rss_n = np.zeros([1463,720])
+    for ij in range(1463):
+        rss_n[ij,0:360] = rss[0,(360*ij):(360*(ij+1))]
+        rss_n[ij,360:] = rss[1,(360*ij):(360*(ij+1))]
+
     stdz = []
     for i in range(len(rss_n)):
-        mn = np.mean(rss[i,:])
-        mx0 = np.std(rss[i,:])
-        rss_n[i,:,0] = (rss[i,:]-mn)/mx0
-        mn = np.mean(rss[i+int(l/wind),:])
-        mx1 = np.std(rss[i+int(l/wind),:])
-        rss_n[i,:,0] = (rss[i+int(l/wind),:]-mn)/mx1
 
-        # adding the substract betw the lobs
-        temp = rss[i,:]+rss[i+int(l/wind),:]
-        mn = np.mean(temp)
-        mx2 = np.std(temp)
-        rss_n[i,:,0] = (temp-mn)/mx2
-        temp = rss[i,:]-rss[i+int(l/wind),:]
-        mn = np.mean(temp)
-        mx3 = np.std(temp)
-        rss_n[i,:,0] = (temp-mn)/mx3
-        if mx3==0:
+        mn = np.mean(rss_n[i,:])
+        mx0 = np.std(rss_n[i,:])
+        rss_n[i,:] = (rss_n[i,:] - mn) / mx0
+        if mx0==0:
             stdz.append(i)
 
     rss_n = np.delete(rss_n,stdz,0)
@@ -79,12 +71,13 @@ for ii in range(5):
     for i in np.arange(0,int(l/360)):
      n = num[i,:]
      b = Counter(n)
-     gt[i] = b.most_common(1)[0][0]
+     gt[i] = np.sign(b.most_common(1)[0][0])
 
     all_data.append(rss_n)
     all_labels.append(gt)
-    for j in range(4):
-     weit[j] = weit[j]+0.25/(sum(gt==j)/len(gt))
+    classs = 2
+    for j in range(classs):
+     weit[j] = weit[j]+(1/classs)/(sum(gt==j)/len(gt))
 
 
 weit = np.round(weit/min(weit))
@@ -95,7 +88,7 @@ count = 0
 count_1 = 0
 
 
-for ii in range(5):
+for ii in range(1):
     rss_n = all_data[ii]
     gt    = all_labels[ii]
     for i in range(len(rss_n)):
@@ -113,26 +106,26 @@ class FN(nn.Module):
 
         # Output size after convolution filter
         # ((w-f+2P)/s) +1
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=10, kernel_size=[5], stride=3, padding=0)
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=40, kernel_size=[5], stride=3, padding=0)
+        self.bn1 = nn.BatchNorm1d(num_features=40)
+        self.conv2 = nn.Conv1d(in_channels=40, out_channels=80, kernel_size=[10], stride=3, padding=0)
+        self.bn11 = nn.BatchNorm1d(num_features=80)
+        self.conv3 = nn.Conv1d(in_channels=80, out_channels=160, kernel_size=[20], stride=3, padding=0)
+        self.bn12 = nn.BatchNorm1d(num_features=160)
+
+
         # Shape= (b_s,12,50,50)
-        self.bn1 = nn.BatchNorm1d(num_features=10)
         # Shape= (b_s,12,50,50)
 
         # Input shape= (b_s,1,50,50)
 
-        self.fc1 = nn.Linear(in_features=119*10, out_features=1000)
-        self.bn2 = nn.BatchNorm1d(1000)
-        self.fc2 = nn.Linear(in_features=1000, out_features = 100)
-        self.bn3 = nn.BatchNorm1d(100)
-        self.fc3 = nn.Linear(in_features=100, out_features = 10)
-        self.bn4 = nn.BatchNorm1d(10)
-        self.fc4 = nn.Linear(in_features=1000, out_features = 500)
-        self.bn5 = nn.BatchNorm1d(500)
-        self.fc5 = nn.Linear(in_features=500, out_features = 250)
-        self.bn6 = nn.BatchNorm1d(250)
-        self.fc6 = nn.Linear(in_features=250, out_features = 100)
-        self.bn7 = nn.BatchNorm1d(100)
-        self.fc7 = nn.Linear(in_features=10, out_features = 4)
+        self.fc1 = nn.Linear(in_features=20*160, out_features=2000)
+        self.bn2 = nn.BatchNorm1d(2000)
+        self.fc2 = nn.Linear(in_features=2000, out_features = 200)
+        self.bn3 = nn.BatchNorm1d(200)
+        self.fc3 = nn.Linear(in_features=200, out_features = 20)
+        self.bn4 = nn.BatchNorm1d(20)
+        self.fc5 = nn.Linear(in_features=20, out_features = 2)
         self.DO  = nn.Dropout(p=0.5)
         self.relu = nn.ReLU()
         self.Lrelu = nn.LeakyReLU()
@@ -143,8 +136,13 @@ class FN(nn.Module):
         output = self.conv1(input)
         output = self.bn1(output)
         output = self.Lrelu(output)
-
-        output = output.view(-1, 10*119)
+        output = self.conv2(output)
+        output = self.bn11(output)
+        output = self.Lrelu(output)
+        output = self.conv3(output)
+        output = self.bn12(output)
+        output = self.Lrelu(output)
+        output = output.view(-1, 160*20)
         output = self.fc1(output)
         output = self.bn2(output)
         output = self.Lrelu(output)
@@ -156,21 +154,7 @@ class FN(nn.Module):
         output = self.bn4(output)
         output = self.Lrelu(output)
         output = self.DO(output)
-        # output = self.fc4(output)
-        # output = self.bn5(output)
-        # output = self.relu(output)
-        # output = self.DO(output)
-        # output = self.fc5(output)
-        # output = self.bn6(output)
-        # output = self.Lrelu(output)
-        # output = self.DO(output)
-        # output = self.fc6(output)
-        # output = self.bn7(output)
-        # output = self.Lrelu(output)
-        output = self.fc7(output)
-
-
-
+        output = self.fc5(output)
         return output
 
 
@@ -188,11 +172,11 @@ train_loader = get_loader(train_sets,weit)
 #     batch_size=250, shuffle=True)
 train_sets = get_loader(train_sets,weit)
 
-torch.manual_seed(3407)
+torch.manual_seed(1)
 model = FN(num_classes=1).to(device)
 
-optimizer     = torch.optim.Adam(model.parameters(), lr=0.01)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=150, gamma=(0.9))
+optimizer     = torch.optim.SGD(model.parameters(), lr=0.001,weight_decay=0.9)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=800, gamma=0.5)
 
 loss_function = nn.CrossEntropyLoss()
 
@@ -209,18 +193,18 @@ for epoch in range(num_epochs):
 
     for i, (seq, labels) in enumerate(train_loader):
         optimizer.zero_grad()
-        seq = seq.permute(0, 2, 1)
+        seq = seq[None, :]
+        seq = seq.permute(1, 0,2)
         seq = seq.float()
         seq = seq.to(device)
         outputs = model(seq)
         labels = labels.long()
         labels = labels.to(device)
-        loss = loss_function(outputs, labels)
-        s_loss[idx] = loss
+        loss = loss_function(outputs, labels)# + 0.05*torch.sqrt(sum(sum(outputs*outputs)))
         idx = idx+1
         loss.backward()
         optimizer.step()
-        scheduler.step()
+        #scheduler.step()
         train_loss += loss.cpu().data * seq.size(0)
         _, prediction = torch.max(outputs.data, 1)
 
@@ -234,7 +218,8 @@ for epoch in range(num_epochs):
     test_accuracy = 0.0
     test_loss     = 0.0
     for i, (seq, labels) in enumerate(test_loader):
-        seq = seq.permute(0, 2, 1)
+        seq = seq[None, :]
+        seq = seq.permute(1, 0,2)
         seq = seq.float()
         seq = seq.to(device)
         labels = labels.long()
